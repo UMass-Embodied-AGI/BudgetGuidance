8ea2b6849c3732e6a322d6b72e922211d40e255c
Junyan Li <lijunyan668@outlook.com>
2025-06-17 16:20:36 -0700

    Update train.sh

--- training/train.sh
+++ training/train.sh
@@ -1,5 +1,5 @@
-export WANDB_API_KEY=7a637fdf546ac2d41295c4c256f5fab6b7d1b647
-export WANDB_PROJECT=ttc
+export WANDB_API_KEY=YOUR_WANDB_API_KEY
+export WANDB_PROJECT=YOUR_WANDB_PROJECT
 
 ACCELERATE_LOG_LEVEL=info accelerate launch --config_file recipes/accelerate_configs/zero3.yaml \
     src/open_r1/sft.py \
